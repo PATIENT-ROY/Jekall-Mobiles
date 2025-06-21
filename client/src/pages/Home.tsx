9a0340fd479b222d33d2dbb0ebc35519db3acc77
Patient-Roy <roy-mbaya@mail.ru>
2025-06-22 03:10:51 +0500

    fiks bug 2

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -238,14 +238,14 @@ const Home = () => {
           </button>
 
           {/* Slide Indicators */}
-          <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+          <div className="absolute bottom-2 sm:bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-0.5 sm:space-x-1">
             {bannerSlides.map((_, index) => (
               <button
                 key={index}
                 onClick={() => setCurrentSlide(index)}
-                className={`w-3 h-3 rounded-full transition-all duration-200 ${
+                className={`w-1 h-1 sm:w-2 sm:h-2 rounded-full transition-all duration-200 ${
                   index === currentSlide
-                    ? "bg-white scale-125"
+                    ? "bg-white"
                     : "bg-white/50 hover:bg-white/75"
                 }`}
               />
